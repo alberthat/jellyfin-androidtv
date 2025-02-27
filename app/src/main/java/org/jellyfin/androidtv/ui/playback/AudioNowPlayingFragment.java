ceebfdb80c51da08911b406624fe757eb90eb241
Albert Hattingh <alberthat@gmail.com>
2025-02-28 07:51:49 +1100

    added lyrics button to Audio Now Playing
    (cherry picked from commit e7034a3cf8f3bf256abc1df06cc912111f967579)

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/AudioNowPlayingFragment.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/AudioNowPlayingFragment.java
@@ -43,6 +43,7 @@ import org.jellyfin.androidtv.util.KeyProcessor;
 import org.jellyfin.androidtv.util.TimeUtils;
 import org.jellyfin.androidtv.util.Utils;
 import org.jellyfin.playback.core.PlaybackManager;
+import org.jellyfin.playback.core.model.LyricsState;
 
 import java.util.List;
 
@@ -59,6 +60,7 @@ public class AudioNowPlayingFragment extends Fragment {
     private ImageButton mRepeatButton;
     private ImageButton mShuffleButton;
     private ImageButton mAlbumButton;
+    private ImageButton mLyricsButton;
     private ImageButton mArtistButton;
     private TextView mCounter;
     private ScrollView mScrollView;
@@ -172,6 +174,17 @@ public class AudioNowPlayingFragment extends Fragment {
             }
         });
         mAlbumButton.setOnFocusChangeListener(mainAreaFocusListener);
+
+        mLyricsButton = binding.lyricsBtn;
+        mLyricsButton.setContentDescription(getString(R.string.lbl_lyrics));
+        mLyricsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaManager.getValue().cycleLyricsState();
+                updateButtons();
+            }
+        });
+        mLyricsButton.setOnFocusChangeListener(mainAreaFocusListener);
 
         mArtistButton = binding.artistBtn;
         mArtistButton.setContentDescription(getString(R.string.lbl_open_artist));
@@ -312,6 +325,7 @@ public class AudioNowPlayingFragment extends Fragment {
     private void updateButtons() {
         Timber.d("Updating buttons");
         boolean playing = mediaManager.getValue().isPlayingAudio();
+        LyricsState lyricsState = mediaManager.getValue().getLyricsState();
         requireActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -332,6 +346,20 @@ public class AudioNowPlayingFragment extends Fragment {
                 if (mBaseItem != null) {
                     mAlbumButton.setEnabled(mBaseItem.getAlbumId() != null);
                     mArtistButton.setEnabled(mBaseItem.getAlbumArtists() != null && !mBaseItem.getAlbumArtists().isEmpty());
+                }
+                switch(lyricsState) {
+                    case OFF:
+                        mLyricsButton.setImageResource(R.drawable.ic_lyrics);
+                        mLyricsButton.setContentDescription("Show lyrics");
+                        break;
+                    case SCROLLING:
+                        mLyricsButton.setImageResource(R.drawable.ic_closed_caption);
+                        mLyricsButton.setContentDescription("Show captions");
+                        break;
+                    case CAPTIONS:
+                        mLyricsButton.setImageResource(R.drawable.ic_closed_caption_off);
+                        mLyricsButton.setContentDescription("Hide captions");
+                        break;
                 }
             }
         });
